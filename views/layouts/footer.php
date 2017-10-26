1791f9ef778cee22abd5899c9fa6112e79edcdba
Henry Vega <hp.vega@hotmail.com>
2017-10-26 00:54:53 -0500

    Eqatu

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,38 +1,32 @@
-<footer class="footer-color">
-    <div class="container">
-        <div class="row">
-            <div class="col l6 s12">
-                <h4>Eqatu</h4>
-                <p class="grey-text text-darken-4">EQATU integra diferentes MYPES en una plataforma virtual, creando
-                    una nueva experiencia de compra fácil,segura y rápida.
-                </p>
-            </div>
-            <div class="col l4 offset-l2 s12">
-                <h4>Redes Sociales</h4>
-                <ul>
-                    <a class="fa fa-facebook-square fa-2x space tooltipped" aria-hidden="true" target="_blank"
-                       data-position="top" data-delay="50" data-tooltip="Facebook Eqatu"
-                       href="https://www.facebook.com/eqatu/"></a>
+<footer class=" footer-color-B">
+    <div class="row">
+        <div class="col s12 m6 center">
+            ¡Contáctenos con nosotros en las redes sociales!
+        </div>
+        <div class="col s12 m6 center">
+            <a class="fa fa-facebook-square fa-lg white-text tooltipped mr-4" aria-hidden="true"
+               target="_blank" data-position="top" data-delay="50" data-tooltip="Facebook Eqatu"
+               href="https://www.facebook.com/eqatu/">
 
-                    <a class="fa fa-twitter fa-2x space tooltipped" aria-hidden="true" target="_blank"
-                       data-position="top" data-delay="50" data-tooltip="Twitter Eqatu"
-                       href="https://twitter.com">
-                    </a>
-
-                    <a class="fa fa-google-plus fa-2x space tooltipped" aria-hidden="true" target="_blank"
-                       data-position="top" data-delay="50" data-tooltip="Google+ Eqatu"
-                       href="https://plus.google.com">
-                    </a>
-                </ul>
-            </div>
+            </a>
+            <a class="fa fa-twitter fa-lg white-text tooltipped mr-4" aria-hidden="true" target="_blank"
+               data-position="top" data-delay="50" data-tooltip="Twitter Eqatu"
+               href="https://twitter.com">
+            </a>
+            <a class="fa fa-google-plus fa-lg white-text tooltipped mr-4" aria-hidden="true" target="_blank"
+               data-position="top" data-delay="50" data-tooltip="Google+ Eqatu"
+               href="https://plus.google.com">
+            </a>
         </div>
     </div>
-    <div class="section white">
+
+    <div class="section footer-color-A">
         <div class="container center">
             Copyright ©<a class="resaltar" href="https://www.facebook.com/eqatu/" target="_blank"> Eqatu </a>2017. Todos
             los Derechos Reservados.
         </div>
     </div>
+
 </footer>
 
 <a href="#" class="back-to-top"><i class="fa fa-chevron-up fa-lg" aria-hidden="true"></i></a>
