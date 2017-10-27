f4c312f85801f7e28b1c0a76fbed21afd7627232
Henry Vega <hp.vega@hotmail.com>
2017-10-27 00:38:14 -0500

    Eqatu

--- views/site/index.php
+++ views/site/index.php
@@ -12,28 +12,39 @@ $this->title = 'Eqatu | Inicio ';
 
 <div class="section white">
     <div class="row container">
-        <h2 class="header">Parallax</h2>
-        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this
-            case, is moved at a different speed than the foreground content while scrolling.</p>
-        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this
-            case, is moved at a different speed than the foreground content while scrolling.</p>
-        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this
-            case, is moved at a different speed than the foreground content while scrolling.</p>
-        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this
-            case, is moved at a different speed than the foreground content while scrolling.</p>
-        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this
-            case, is moved at a different speed than the foreground content while scrolling.</p>
-        <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this
-            case, is moved at a different speed than the foreground content while scrolling.</p>     <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this
-            case, is moved at a different speed than the foreground content while scrolling.</p>     <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this
-            case, is moved at a different speed than the foreground content while scrolling.</p>
+        <h2 class="header center">Quiero Promocionar mi Producto</h2>
+        <h3 class="subheader center">Que tengo que hacer</h3>
+        <div class="row center">
+            <div class="col s12 m4">
+                <i class="fa fa-newspaper-o fa-5x"></i>
+            </div>
+            <div class="col s12 m4">
+                <i class="fa fa-calculator fa-5x"></i>
+            </div>
+            <div class="col s12 m4">
+                <i class="fa fa-desktop fa-5x"></i>
+            </div>
+        </div>
 
-
+        <div class="row center">
+            <div class="col s12 m4 ">
+                <i class="fa fa-cart-plus fa-5x"></i>
+            </div>
+            <div class="col s12 m4">
+                <i class="fa fa-truck fa-5x"></i>
+            </div>
+            <div class="col s12 m4">
+                <i class="fa fa-credit-card-alt fa-5x"></i>
+            </div>
+        </div>
     </div>
 </div>
 
-<div class="parallax-container">
-    <div class="parallax"><img src="http://materializecss.com/images/parallax2.jpg"></div>
+<div class="section footer-color-C">
+    <div class="row container">
+        <div class="col s12 m6 center">6-columns (one-half)</div>
+        <div class="col s12 m6 center">6-columns (one-half)</div>
+    </div>
 </div>
 
 <section id="contact">
