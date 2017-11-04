a40e0ce096f29d5e18fc3c824b029bff377510dc
Henry Vega <hp.vega@hotmail.com>
2017-11-04 15:02:46 -0500

    Eqatu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -27,7 +27,19 @@ AppAsset::register($this);
 <?php $this->render('header') ?>
 <?= $content ?>
 <?= $this->render('footer') ?>
-
+<script>
+    (function(d, w, c) {
+        w.ChatraID = 'pCQqmEePgqhuHYRsx';
+        var s = d.createElement('script');
+        w[c] = w[c] || function() {
+                (w[c].q = w[c].q || []).push(arguments);
+            };
+        s.async = true;
+        s.src = (d.location.protocol === 'https:' ? 'https:': 'http:')
+            + '//call.chatra.io/chatra.js';
+        if (d.head) d.head.appendChild(s);
+    })(document, window, 'Chatra');
+</script>
 <?php $this->endBody() ?>
 <?= Html::jsFile('@web/js/all.js?v=' . filemtime(Yii::getAlias('@webroot/js/all.js'))) ?>
 </body>
